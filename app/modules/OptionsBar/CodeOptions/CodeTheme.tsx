c7898608ae37cd5ea3625beca0295e61004b1a6a
Hoon Choi <vhchoi@gmail.com>
2016-12-09 14:33:13 -0800

    feat: Options for text editor

---
+++ app/modules/OptionsBar/CodeOptions/CodeTheme.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Menu, MenuDivider, MenuItem, Popover, Position } from "@blueprintjs/core";
+
+const brace = require('brace');
+const AceEditor = require('react-ace').default;
+
+interface CodeThemeProps {
+  pluginState: any;
+  updateCurrentPlugin: Function;
+}
+
+const CodeTheme = ({ pluginState, updateCurrentPlugin }: CodeThemeProps) => {
+  const DEFAULT_THEME = 'Monokai';
+
+  const themes = ['Ambiance', 'Chaos', 'Chrome', 'Clouds', 'Cobalt', 'Eclipse', 'iPlastic', 'Monokai', 'Textmate', 'Tomorrow', 'Twilight', 'XCode'];
+
+  const themeSelection = (
+    <Menu>
+        {
+          themes.map((theme, key) => (
+            <MenuItem
+              key={ key }
+              text={ theme }
+              onClick={() => updateCurrentPlugin({ theme: theme })} />
+          ))
+        }
+    </Menu>
+  );
+
+  return (
+    <Popover content={ themeSelection } position={ Position.RIGHT_TOP }>
+      <button className="pt-button" type="button">
+        { pluginState.theme ? pluginState.theme : DEFAULT_THEME }
+      </button>
+    </Popover>
+  );
+}
+
+export default CodeTheme;
--- app/plugins/CodeEditor/CodeEditor.tsx
+++ app/plugins/CodeEditor/CodeEditor.tsx
@@ -5,8 +5,33 @@ import './codeEditor.scss';
 const brace = require('brace');
 const AceEditor = require('react-ace').default;
 
+//languages
+require('brace/mode/c_cpp');
+require('brace/mode/clojure');
+require('brace/mode/django');
+require('brace/mode/haskell');
+require('brace/mode/html');
 require('brace/mode/javascript');
+require('brace/mode/json');
+require('brace/mode/perl');
+require('brace/mode/php');
+require('brace/mode/python');
+require('brace/mode/ruby');
+require('brace/mode/typescript');
+
+//themes
+require('brace/theme/ambiance');
+require('brace/theme/chaos');
+require('brace/theme/chrome');
+require('brace/theme/clouds');
+require('brace/theme/cobalt');
+require('brace/theme/eclipse');
+require('brace/theme/iplastic');
 require('brace/theme/monokai');
+require('brace/theme/textmate');
+require('brace/theme/tomorrow');
+require('brace/theme/twilight');
+require('brace/theme/xcode');
 
 interface CodeEditorProps {
   height: number;
@@ -19,14 +44,22 @@ interface CodeEditorProps {
 
 const CodeEditor = ({ height, width, pluginNumber, pluginState, slideNumber, updateCurrentSlide }: CodeEditorProps) => {
   const DEFAULT_FONT_SIZE = 8;
+  const DEFAULT_LANGUAGE = 'javascript';
+  const DEFAULT_THEME = 'monokai';
 
-  const { fontSize, snippet, snippetEval } = pluginState;
+  const { fontSize, language, snippet, snippetEval, theme } = pluginState;
+  let setLang: string;
+  if(language === 'C++') setLang = 'c_cpp';
+  else if (language) setLang = language.toLowerCase();
+
+  let setTheme: string;
+  if (theme) setTheme = theme.toLowerCase();
 
   return (
     <div>
-      <AceEditor 
-        mode='javascript'
-        theme='monokai'
+      <AceEditor
+        mode={ setLang ? setLang : DEFAULT_LANGUAGE }
+        theme={ setTheme? setTheme : DEFAULT_THEME }
         tabSize={2}
         fontSize={ fontSize ? DEFAULT_FONT_SIZE * (fontSize / 100) : DEFAULT_FONT_SIZE * 3 }
         height={`${height}px`}
